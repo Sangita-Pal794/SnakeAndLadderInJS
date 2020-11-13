335f3787bc35ba9d0cf8cf82413bdcc38aba67cd
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-13 11:14:23 +0530

    Function to decide which type of move

--- Utility.js
+++ Utility.js
@@ -1,6 +1,28 @@
-module.exports = {getRandom}
+module.exports = {checkOption}
+var WinPos=100
+var StartPos=0
+const MOVE_TYPE_DETAILS = {
+
+    NOPLAY: 1,
+    SNAKE:  2,
+    LADDER: 3,
+};
+Object.freeze = MOVE_TYPE_DETAILS;
 function getRandom(){
 let Dice = ( 1 + Math.floor(Math.random() * 6))
     return Dice
 }
-getRandom()
+let move_type = Math.floor(Math.random() * 4);
+function checkOption  () {
+	switch (move_type) {  
+            case 1:
+            return (StartPos+0);
+            case 2:
+            return (StartPos+getRandom());
+            case 3:
+            return (StartPos-getRandom());
+            default:
+            return 0;
+    }
+};
+
--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,6 +1,4 @@
 const Utility = require('./Utility');
-let result = Utility.getRandom();
+console.log(Utility.checkOption());
 console.log("Welcome To Snake And Ladder!!") 
-console.log(result)
-var WinPos=100
-var StartPos=0;
+
