6f8d5deb47817d3aa654dd48d50c75cabb330d60
Sangita-Pal794 <pal07sangita@gmail.com>
2020-11-17 09:38:44 +0530

    function to print the number of times played and output ……of each turn

--- Utility.js
+++ Utility.js
@@ -1,7 +1,6 @@
 const WinPos=100
 var StartPos=0
 const MOVE_TYPE_DETAILS = {
-
     NOPLAY: 1,
     SNAKE:  2,
     LADDER: 3,
@@ -25,27 +24,62 @@ function checkOption  () {
     }
 }
 let PlayerOnePos=StartPos
-let dice1count =0
-function play() {
-while(PlayerOnePos <= WinPos)
+let PlayerTwoPos=StartPos
+function play(PlayerPos,dicecount) {
+  while(PlayerPos < WinPos)
     {
-    PlayerOnePos = getRandom()
-    dice1count++
-    let TempPos=(checkOption (PlayerOnePos));
-    PlayerOnePos=TempPos
-    if (PlayerOnePos == WinPos){
-        console.log("Player One is currently at",WinPos,"after throwing",dice1count,"times")
-        exit
+    PlayerPos = getRandom()
+    dicecount++
+    let TempPos=(checkOption (PlayerPos));
+    PlayerPos=TempPos
+    if (PlayerPos == WinPos){
+        console.log("Player One is currently at",WinPos,"after throwing",dicecount,"times")
+        break
     }
-else if (PlayerOnePos >= WinPos){
-        console.log("Player One is currently at",PlayerOnePos,"after throwing",dice1count,"times")
-        PlayerOnePos=(PlayerOnePos-Dice)
+    else if (PlayerOnePos >= WinPos){
+        console.log("Player One is currently at",PlayerPos,"after throwing",dicecount,"times")
+        PlayerPos=(PlayerPos-Dice)
+        break
 }
 else {
-    PlayerOnePos=StartPos
+    PlayerPos=StartPos
+    console.log("PLAYER One IS AT",PlayerPos)
 }
 }
 } 
-
-module.exports = {play,checkOption,getRandom}
-
+let dice1count=0
+function playerone(){
+	while(PlayerOnePos < WinPos){
+		play(PlayerOnePos,dice1count)
+		while(move_type == "LADDER"){
+		    play(PlayerOnePos,dice1count)
+        playertwo()
+    }
+    console.log(PlayerOnePos)
+    console.log(dice1count)
+    }
+}
+let dice2count=0
+function playertwo(){
+	while(PlayerTwoPos < WinPos){
+		play(PlayerTwoPos,dice2count)
+		while(move_type == "LADDER"){
+		    play(PlayerOnePos,dice2count)
+        playerone()
+    }
+    console.log(PlayerTwoPos)
+    console.log(dice2count)
+    }
+}
+function Compare(PlayerOnePos,PlayerTwoPos){
+    if(PlayerOnePos>PlayerTwoPos)
+    return (dice1count-dice2count)
+    //console.log("PlayerOne wins by",(dice1count-dice2count),"times")
+    else if(PlayerTwoPos>PlayerOnePos)
+    return (dice2count-dice1count)
+    //console.log("PlayerTwo wins by",(dice2count-dice1count),"times")
+    else
+    console.log("This match is draw.Play again!!")
+}
+playerone()
+module.exports = {play,checkOption,getRandom,playerone,playertwo,Compare}
--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,4 +1,5 @@
 const Utility = require('./Utility');
-console.log("Welcome To Snake And Ladder!!") 
-console.log("Player One is currently at",Utility.PlayerOnePos,"after throwing",Utility.dice1count, "times")
+//console.log("Welcome To Snake And Ladder!!") 
+const diff = Utility.Compare();
+console.log("Winner Player is won by",diff)
 
