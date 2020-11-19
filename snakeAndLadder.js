38f9be4b2882aac3b3188663bcb0e0a7b11b6e5f
Sangita <pal07sangita@gmail.com>
2020-11-19 11:39:51 +0530

    Refactored:Play the game to decide the winner

--- snakeAndLadder.js
+++ snakeAndLadder.js
@@ -1,5 +1,5 @@
 const Utility = require('./Utility');
-//console.log("Welcome To Snake And Ladder!!") 
+console.log("Welcome To Snake And Ladder!!") 
 const diff = Utility.Compare();
 console.log("Winner Player is won by",diff)
 
